8d539d046627995b2d13f6420afa0da9369a77c6
Charles Frye <cfrye59@gmail.com>
2023-05-23 10:41:22 -0700

    fake backend

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+import modal
+
+image = modal.Image.debian_slim(python_version="3.10")
+
+stub = modal.Stub(name="riffusion")
+
+
+@stub.function(
+    image=image,
+)
+@modal.web_endpoint(method="POST", label="riff")
+def inference(input: dict):
+    return {"url": "https://riff-store.s3.us-west-2.amazonaws.com/test.mp3"}
